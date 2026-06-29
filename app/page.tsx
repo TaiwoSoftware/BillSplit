a0404657887d1e32686847f77cbe37f94e42a3d8
TaiwoSoftware <davcodes24@gmail.com>
2026-06-29 19:31:28 +0100

    testing again

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Badge from "@/app/components/ui/Badge";
 import Button from "@/app/components/ui/Button";
 import Card from "@/app/components/ui/Card";
 import Progress from "@/app/components/ui/Progress";
-import TestConnection from "./components/TestConnection";
 
 export default function Home() {
   const features = [
@@ -74,7 +73,6 @@ export default function Home() {
           </div>
         </nav>
       </header>
-      <TestConnection />
 
       {/* Hero Section */}
       <section className="mx-auto grid max-w-7xl gap-16 px-6 py-24 lg:grid-cols-2 lg:items-center">
